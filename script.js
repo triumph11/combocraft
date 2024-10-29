3f4869c37d226e1dc03bbada84c0972ac2fdfe98
triumph11 <130575743+triumph11@users.noreply.github.com>
2024-10-29 09:30:26 -0600

    Add files via upload
    new files

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById("downloadBtn").addEventListener("click", function() {
+    window.location.href = "https://www.curseforge.com/minecraft/mc-mods/simple-voice-chat/download/5850267";
+});
